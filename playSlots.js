8a73257901b26ab93b37743e0895389c276f14eb
loyti <brice@loyti.com>
2017-06-26 11:25:07 -0700

    updated js file

--- playSlots.js
+++ playSlots.js
@@ -2,26 +2,28 @@ var game;
 var games;
 var win;
 var lose;
-var quarter = 25;
-var bank = Math.round((Math.random(100)*100));
-var odds = Math.round((Math.random(100)*100));
+var quarter = Math.round((Math.random(100)*100));
 function playSlots () {
+    var odds = Math.round((Math.random(100)*100));
     document.write("<h1>Let's Play with a Slot Machine</h1>");
     document.write("<p>You are beginning with " + quarter + " in the bank.</p>");
     document.write("<p>To play it will cost you 1 quarter from the bank.</p>");
-    
-    game = bank - 1;
-    for (var i = 0; i < quarter; i++) {
-        if (odds % 10 == 0) {
-            if ( game > 0){
-                win = true;
-                lose = false;
-                bank++;
-                document.write("<p class=\"win\">Congratulations you won! You now have this many " + bank + " left</p>");
-            } else {
-                document.write("<p class=\"try\">You are out of money ... refresh the page to try again.</p>");
-            }
-        }                
+    document.write("<p>Your odds of winning right now are: " + odds + " ... Good luck :)</p>")
+    while(quarter > 0){
+        odds = Math.round((Math.random(100)*100));
+        if (odds > 90){
+            win = true;
+            lose = false;
+            quarter++;
+            odds = Math.round((Math.random(100)*100));
+            document.write("<p class=\"win\">Congratulations you won! You now have this many " + quarter + " left</p>");
+        } else {
+            win = false; 
+            lose = true;
+            quarter--;
+            odds = Math.round((Math.random(100)*100));
+            document.write("<p class=\"try\">You lost. Count your quarters closely ... you only have " + quarter + " left</p>");
+        }
     }
 }
 playSlots();
